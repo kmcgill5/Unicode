ac273710efeb382302f7083baaee9242ebe88c36
kmcgill5 <149637996+kmcgill5@users.noreply.github.com>
2024-09-18 22:17:51 -0400

    Add files via upload

---
+++ Unicode.java
@@ -0,0 +1,19 @@
+public class Unicode
+{
+    public static void main(String[] args)
+    {
+        First one = new First();
+        Second two = new Second();
+        Third three = new Third();
+        Fourth four = new Fourth();
+        
+        one.uni();
+        one.code();
+        two.uni();
+        two.code();
+        three.uni();
+        three.code();
+        four.uni();
+        four.code();
+    }
+}
